0988501a51808eb27472d260c6b444c10e38257b
irapanch <117355580+irapanch@users.noreply.github.com>
2023-09-18 13:36:37 +0300

    refact

--- src/App.js
+++ src/App.js
@@ -1,78 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyledApp } from './Apps.Style';
-// import AddFeedback from './components/AddFeedback.jsx';
+
 import Statistics from './components/Statistics.jsx';
 import Section from './components/Section.jsx';
 import Notification from './components/Notification.jsx';
 import FeedbackOptions from 'components/FeedbackOptions';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleBtnClick = item => {
+    if (item === 'good') {
+      setGood(prev => prev + 1);
+    } else if (item === 'neutral') {
+      setNeutral(prev => prev + 1);
+    } else if (item === 'bad') {
+      setBad(prev => prev + 1);
+    }
   };
 
-  // handleAddGood = () => {
-  //   this.setState(prevState => ({
-  //     good: prevState.good + 1,
-  //   }));
-  // };
-
-  // handleAddNeutral = () => {
-  //   this.setState(prevState => ({
-  //     neutral: prevState.neutral + 1,
-  //   }));
-  // };
-
-  // handleAddBad = () => {
-  //   this.setState(prevState => ({
-  //     bad: prevState.bad + 1,
-  //   }));
-  // };
-  arrForMarkup = Object.keys(this.state);
-
-  handleBtnClick = item => {
-    this.setState(prev => ({ [item]: prev[item] + 1 }));
-  };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     return total === 0 ? 0 : ((good / total) * 100).toFixed(0);
   };
 
-  render() {
-    return (
-      <StyledApp>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            arrForMarkup={this.arrForMarkup}
-            handleBtnClick={this.handleBtnClick}
-            // addGood={this.handleAddGood}
-            // addNeutral={this.handleAddNeutral}
-            // addBad={this.handleAddBad}
-          />
-        </Section>
+  const arrForMarkup = ['good', 'neutral', 'bad'];
 
-        <Section title="Statistics">
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              state={this.state}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </StyledApp>
-    );
-  }
-}
+  return (
+    <StyledApp>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          arrForMarkup={arrForMarkup}
+          handleBtnClick={handleBtnClick}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            state={{ good, neutral, bad }}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </StyledApp>
+  );
+};
 
 export default App;
